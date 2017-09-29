cc95993c1fb62413e530a1b906cfe2c576512e01
Chris <forexampleman@gmail.com>
2017-09-29 19:43:17 -0400

    Basic user functionality with secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925164638) do
+ActiveRecord::Schema.define(version: 20170929230440) do
 
   create_table "cube_cards", force: :cascade do |t|
     t.integer "card_id"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20170925164638) do
     t.datetime "create_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

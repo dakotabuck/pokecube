d37134b463929ae25dcd3e5a1b3be01d3d005021
DakotaBuck <firebrick22@gmail.com>
2017-09-30 18:11:38 -0400

    Massive Query In Migration
    Check migrate\insert_into_cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930203217) do
+ActiveRecord::Schema.define(version: 20170929230440) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -58,9 +58,6 @@ ActiveRecord::Schema.define(version: 20170930203217) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "dob"
-    t.binary "perms"
-    t.datetime "account_date"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

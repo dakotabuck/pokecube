d8edb30784e5f8c7aeba5ec1bce29de2c4731bef
DakotaBuck <firebrick22@gmail.com>
2017-10-07 01:11:27 -0400

    Change to CubeCards
    Added the ability to Destroy any CubeCard

--- app/controllers/cube_cards_controller.rb
+++ app/controllers/cube_cards_controller.rb
@@ -5,6 +5,13 @@ class CubeCardsController < ApplicationController
     redirect_to cube_path(@cube)
   end
 
+  def destroy
+    @cube = Cube.find(params[:cube_id])
+    @cube_card = @cube.cube_cards.find(params[:id])
+    @cube_card.destroy
+    redirect_to cube_path(@cube)
+  end
+
   private
     def cube_card_params
       params.require(:cube_card).permit(:cube_id, :card_id)

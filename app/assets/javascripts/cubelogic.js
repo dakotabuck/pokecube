04816164085a3171d721bd53cb610c6371a238d8
DakotaBuck <firebrick22@gmail.com>
2017-12-16 23:40:00 -0500

    Fixed Search Script
    Implemented AJAX call for search script

--- app/assets/javascripts/cubelogic.js
+++ app/assets/javascripts/cubelogic.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
   $("#selection-card input").change(function () {
-    $("#selection-card input").
+    //searchText = $("#selection-card input");
+    //$('#card-preview').attr('src', "https://images.pokemontcg.io/" + searchText[0].options[inputs[0].selectedIndex].text + "/" + inputs[1].value + ".png")
+    var inputs = document.querySelectorAll("#selection-card input");
+    document.querySelector('#card-preview').src = "https://images.pokemontcg.io/" + inputs[0].options[inputs[0].selectedIndex].text + "/" + inputs[1].value + ".png";
+  });
+  $("#card-search-field").change(function () {
+    $.get("/cards/" + $("#card-search-field").val(), function (d) {
+      $("#card-search").html(d);
+    });
   });
 });
